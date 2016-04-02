db21bc6fb4bdc49cdd7125251f25ab7a3a8cd929
LuizIFC <luiz.allievi@inf.concordia.ifc.edu.br>
2016-04-02 09:36:33 -0300

    commit 20

--- js/script.js
+++ js/script.js
@@ -1,3 +1,22 @@
-/**
- * Created by root on 02/04/16.
- */
+$( document ).ready(
+    function () {
+
+
+    }
+)
+
+var jObject = {
+    Questions = [
+    {
+        "question" : "labaklba",
+        "anwers" : [
+            "answer 1",
+            "asnwer 2",
+            "asnwer 3",
+            "answer 4",
+        ]
+        //here goes the index of the correct answer in answers list
+        "correct_aswer" : 2
+    }
+]
+}
